c183406cd01fa03cb473469c70cdee76972300e4
Antoine Biard <antoine.biard@padam.io>
2024-03-12 18:20:25 +0100

    Fix esc combo not working with home rows modifiers

--- keyboards/ferris/keymaps/antoan2_coding/keymap.c
+++ keyboards/ferris/keymaps/antoan2_coding/keymap.c
@@ -29,7 +29,7 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BL] = LAYOUT(
         KC_Q, KC_W, KC_E, KC_R, KC_T,                        KC_Y, KC_U, KC_I,    KC_O,   KC_P,
-        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), HYPR_T(KC_G),      HYPR_T(KC_H), RSFT_T(KC_J), RCTL_T(KC_K),    RALT_T(KC_L),   LGUI_T(KC_SCLN),
+        KC_A, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), LGUI_T(KC_G),      RGUI_T(KC_H), RSFT_T(KC_J), RCTL_T(KC_K),    RALT_T(KC_L),   KC_SCLN,
         KC_Z, KC_X, KC_C, KC_V, KC_B,                        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
                     LT(3, KC_TAB), LT(1, KC_BACKSPACE),       LT(2, KC_SPACE), KC_ENTER
     ),
