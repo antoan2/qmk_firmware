5f49128e5716394f1162ef0f5715dd2347e7d805
Antoine Biard <antoine.biard@padam.io>
2024-03-18 22:37:11 +0100

    New symbol layer

--- keyboards/ferris/keymaps/antoan2_coding/keymap.c
+++ keyboards/ferris/keymaps/antoan2_coding/keymap.c
@@ -29,20 +29,20 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BL] = LAYOUT(
         KC_Q, KC_W, KC_E, KC_R, KC_T,                        KC_Y, KC_U, KC_I,    KC_O,   KC_P,
-        KC_A, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), LGUI_T(KC_G),      RGUI_T(KC_H), RSFT_T(KC_J), RCTL_T(KC_K),    RALT_T(KC_L),   KC_SCLN,
+        KC_A, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), LGUI_T(KC_G),      RGUI_T(KC_H), RSFT_T(KC_J), RCTL_T(KC_K),    LALT_T(KC_L),   KC_SCLN,
         KC_Z, KC_X, KC_C, KC_V, KC_B,                        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
                     LT(3, KC_TAB), LT(1, KC_BACKSPACE),       LT(2, KC_SPACE), KC_ENTER
     ),
 	[_NAV] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE,    KC_LEFT_PAREN,   KC_RIGHT_PAREN,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_LEFT, KC_DOWN,             KC_UP,                   KC_RIGHT,        KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_LEFT_BRACKET,     KC_RIGHT_BRACKET,        KC_QUOTE,        KC_DOUBLE_QUOTE,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_LEFT, KC_DOWN,             KC_UP,                   KC_RIGHT,        KC_DOUBLE_QUOTE,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS,     KC_TRNS,       KC_TRNS,        KC_TRNS,
                                                KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS
     ),
 	[_SYM] = LAYOUT(
-        KC_EXCLAIM,    KC_AT,        KC_HASH,     KC_DOLLAR,     KC_PERCENT,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_UNDERSCORE, KC_PLUS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TILDE,      KC_GRAVE,     KC_PIPE,     KC_EQUAL,      KC_MINUS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TILDE,    KC_AT,        KC_HASH,     KC_DOLLAR,     KC_PERCENT,      KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_QUESTION, KC_DOUBLE_QUOTE,
+        KC_EXCLAIM,KC_LEFT_PAREN, KC_LEFT_BRACKET, KC_UNDERSCORE, KC_MINUS, KC_PLUS, KC_EQUAL, KC_RIGHT_BRACKET, KC_RIGHT_PAREN, KC_QUOTE ,
+        KC_GRAVE,      KC_TRNS,     KC_LEFT_CURLY_BRACE,     KC_LEFT_ANGLE_BRACKET,      KC_TRNS,         KC_TRNS, KC_RIGHT_ANGLE_BRACKET, KC_RIGHT_CURLY_BRACE, KC_PIPE, KC_BACKSLASH,
                                                               KC_TRNS,       KC_TRNS,          KC_TRNS, KC_TRNS
     ),
 [_NUM] = LAYOUT(
