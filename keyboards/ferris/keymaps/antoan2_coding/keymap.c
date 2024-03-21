44a4c5b4655822b954e893a2170aabb13e28598b
Antoine Biard <antoine.biard@padam.io>
2024-03-21 09:12:43 +0100

    add mouse layer, media control

--- keyboards/ferris/keymaps/antoan2_coding/keymap.c
+++ keyboards/ferris/keymaps/antoan2_coding/keymap.c
@@ -12,6 +12,7 @@ enum layer_names {
     _NAV,
     _SYM,
     _NUM,
+    _MOUSE,
 };
 
 // Almost exactly https://peppe.rs/posts/programming_on_34_keys/
@@ -28,15 +29,15 @@ enum layer_names {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BL] = LAYOUT(
-        KC_Q,         KC_W,         KC_E,           KC_R,         KC_T,      KC_Y, KC_U,         KC_I,            KC_O,           KC_P,
-        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D),   LSFT_T(KC_F), KC_G,      KC_H, RSFT_T(KC_J), RCTL_T(KC_K),    LALT_T(KC_L),   LGUI_T(KC_SCLN),
+        KC_Q,         KC_W,         KC_E,           KC_R,         KC_T,      KC_Y, KC_U,         LT(4, KC_I),            KC_O,           KC_P,
+        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D),   LSFT_T(KC_F), KC_G,      KC_H, RSFT_T(KC_J), RCTL_T(KC_K),    LALT_T(KC_L),   RGUI_T(KC_SCLN),
         KC_Z,         KC_X,         KC_C,           KC_V,         KC_B,      KC_N, KC_M,         KC_COMM,         KC_DOT,         KC_SLSH,
                     LT(3, KC_TAB), LT(1, KC_BACKSPACE),       LT(2, KC_SPACE), KC_ENTER
     ),
 	[_NAV] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_TRNS,               KC_TRNS, KC_MS_WH_UP,  KC_MS_WH_DOWN, KC_TRNS,  KC_TRNS,
-        KC_VOLD, KC_VOLU, KC_MUTE,             KC_MEDIA_PLAY_PAUSE, KC_TRNS,               KC_LEFT, KC_DOWN,      KC_UP,         KC_RIGHT, KC_HOME,
-        KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS,             KC_TRNS,               KC_HOME, KC_PAGE_DOWN, KC_PAGE_UP,    KC_TRNS,  KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_MUTE, KC_MEDIA_PLAY_PAUSE, KC_TRNS,               KC_TRNS, KC_MS_WH_DOWN,  KC_MS_WH_UP, KC_TRNS,  KC_TRNS,
+        KC_MEDIA_PREV_TRACK, KC_VOLU, KC_VOLD, KC_MEDIA_NEXT_TRACK, KC_TRNS,               KC_LEFT, KC_DOWN,      KC_UP,         KC_RIGHT, KC_TRNS,
+        KC_MEDIA_REWIND, KC_TRNS, KC_TRNS,             KC_TRNS,             KC_TRNS,               KC_HOME, KC_PAGE_DOWN, KC_PAGE_UP,    KC_END,  KC_TRNS,
                                                            KC_TRNS,             KC_TRNS,               KC_TRNS, KC_TRNS
     ),
 	[_SYM] = LAYOUT(
@@ -51,10 +52,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_0,    KC_1, KC_2, KC_3, KC_TRNS,
                                                KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS
     ),
+    [_MOUSE] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_MS_WH_UP, KC_TRNS, KC_TRNS,               KC_TRNS, KC_MS_BTN1, KC_TRNS, KC_MS_BTN2, KC_TRNS,
+        KC_MS_LEFT, KC_MS_UP, KC_MS_DOWN, KC_MS_RIGHT, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                    KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS
+    ),
 };
 
-const uint16_t PROGMEM esc_combo[] = {KC_A, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM cap_word_combo[] = {KC_Z, KC_SLASH, COMBO_END};
+
+const uint16_t PROGMEM esc_combo[] = {LCTL_T(KC_D), RCTL_T(KC_K), COMBO_END};
+const uint16_t PROGMEM cap_word_combo[] = {LSFT_T(KC_F), RSFT_T(KC_J), COMBO_END};
 combo_t key_combos[] = {
     COMBO(esc_combo, KC_ESC),
     COMBO(cap_word_combo, QK_CAPS_WORD_TOGGLE),
