e265c59b4f5c1e6de41f33b224cd8c1fc7ebeeab
Antoine Biard <antoine.biard@padam.io>
2024-03-25 23:08:42 +0100

    minor changes for better mouse control

--- keyboards/ferris/keymaps/antoan2_coding/keymap.c
+++ keyboards/ferris/keymaps/antoan2_coding/keymap.c
@@ -32,30 +32,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,         KC_W,         LT(4, KC_E),    KC_R,         KC_T,          KC_Y, KC_U,         KC_I,            KC_O,           KC_P,
         LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D),   LSFT_T(KC_F), KC_G,          KC_H, RSFT_T(KC_J), RCTL_T(KC_K),    LALT_T(KC_L),   RGUI_T(KC_SCLN),
         KC_Z,         KC_X,         KC_C,           KC_V,         KC_B,          KC_N, KC_M,         KC_COMM,         KC_DOT,         KC_SLSH,
-                                    LT(3, KC_TAB), LT(1, KC_BACKSPACE),          LT(2, KC_SPACE), KC_ENTER
+                                        LT(3, KC_TAB), LT(2, KC_BACKSPACE),          LT(1, KC_SPACE), KC_ENTER
     ),
 	[_NAV] = LAYOUT(
-        KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,  KC_VOLU,     KC_VOLD,                KC_TRNS,      KC_TRNS, KC_MS_WH_DOWN, KC_MS_WH_UP,  KC_TRNS,    KC_TRNS,
-        KC_LEFT_GUI,         KC_LEFT_ALT,       KC_LEFT_CTRL,        KC_LEFT_SHIFT,          KC_TRNS,      KC_TRNS, KC_DOWN,       KC_UP,      KC_LEFT,      KC_RIGHT,
-        KC_MEDIA_REWIND, KC_MEDIA_FAST_FORWARD,     KC_MUTE,     KC_MEDIA_PLAY_PAUSE,  KC_TRNS,      KC_TRNS, KC_HOME,       KC_PAGE_DOWN, KC_PAGE_UP, KC_END,
-                                                           KC_TRNS,             KC_TRNS,               KC_TRNS, KC_TRNS
+        KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,   KC_VOLU,       KC_VOLD,                KC_TRNS,      KC_TRNS, KC_MS_WH_DOWN, KC_MS_WH_UP,  KC_MS_WH_LEFT, KC_MS_WH_RIGHT,
+        KC_LEFT_GUI,         KC_LEFT_ALT,           KC_LEFT_CTRL,  KC_LEFT_SHIFT,          KC_TRNS,      KC_TRNS, KC_DOWN,       KC_UP,        KC_LEFT,       KC_RIGHT,
+        KC_MEDIA_REWIND,     KC_MEDIA_FAST_FORWARD, KC_MUTE,       KC_MEDIA_PLAY_PAUSE,    KC_TRNS,      KC_TRNS, KC_PAGE_DOWN,  KC_PAGE_UP,   KC_HOME,       KC_END,
+                                                                               KC_TRNS,                KC_TRNS,      KC_TRNS, KC_TRNS
     ),
 	[_SYM] = LAYOUT(
-        KC_TILDE,  KC_AT,               KC_HASH,               KC_DOLLAR,     KC_PERCENT,      KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK,            KC_QUESTION,          KC_BACKSLASH,
-        KC_EXCLAIM,KC_LEFT_PAREN,       KC_LEFT_BRACKET,       KC_UNDERSCORE, KC_EXCLAIM,      KC_TRNS,       KC_EQUAL,     KC_RIGHT_BRACKET,       KC_RIGHT_PAREN,       KC_DOUBLE_QUOTE ,
-        KC_GRAVE,  KC_LEFT_CURLY_BRACE, KC_LEFT_ANGLE_BRACKET, KC_MINUS,      KC_PIPE,         KC_TRNS,       KC_PLUS,      KC_RIGHT_ANGLE_BRACKET, KC_RIGHT_CURLY_BRACE, KC_QUOTE,
+        KC_TILDE,  KC_AT,               KC_HASH,               KC_DOLLAR,     KC_PERCENT,      KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK,            KC_QUESTION,          KC_COLON,
+        KC_EXCLAIM,KC_LEFT_PAREN,       KC_LEFT_BRACKET,       KC_UNDERSCORE, KC_MINUS,        KC_PLUS,       KC_EQUAL,     KC_RIGHT_BRACKET,       KC_RIGHT_PAREN,       KC_DOUBLE_QUOTE,
+        KC_GRAVE,  KC_LEFT_CURLY_BRACE, KC_LEFT_ANGLE_BRACKET, KC_BACKSLASH,  KC_PIPE,         KC_TRNS,       KC_TRNS,      KC_RIGHT_ANGLE_BRACKET, KC_RIGHT_CURLY_BRACE, KC_QUOTE,
                                                                            KC_TRNS,       KC_TRNS,         KC_TRNS,       KC_TRNS
     ),
     [_NUM] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_0,    KC_4, KC_5, KC_6, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_7, KC_8, KC_9, KC_DOT,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_0,    KC_4, KC_5, KC_6, KC_COMMA,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS,
                                                KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS
     ),
     [_MOUSE] = LAYOUT(
-        KC_TRNS, KC_MS_BTN1, KC_TRNS, KC_MS_BTN2, KC_TRNS,             QK_DYNAMIC_TAPPING_TERM_UP,  KC_MS_WH_UP, KC_TRNS,       KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,             QK_DYNAMIC_TAPPING_TERM_DOWN,  KC_MS_UP,    KC_MS_DOWN,    KC_MS_LEFT,    KC_MS_RIGHT,
-        KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,             QK_DYNAMIC_TAPPING_TERM_PRINT,  KC_MS_WH_UP, KC_MS_WH_DOWN, KC_TRNS,     KC_TRNS,
+        KC_TRNS, KC_MS_BTN1, KC_TRNS, KC_MS_BTN2, KC_TRNS,             QK_DYNAMIC_TAPPING_TERM_UP,    KC_MS_WH_DOWN, KC_MS_WH_UP, KC_MS_WH_LEFT, KC_MS_WH_RIGHT,
+        KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,             QK_DYNAMIC_TAPPING_TERM_DOWN,  KC_MS_DOWN,    KC_MS_UP,    KC_MS_LEFT,    KC_MS_RIGHT,
+        KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,             QK_DYNAMIC_TAPPING_TERM_PRINT, KC_MS_WH_DOWN, KC_MS_WH_UP, KC_TRNS,       KC_TRNS,
                                                   KC_TRNS,    KC_TRNS,             KC_TRNS,  KC_TRNS
     ),
 };
