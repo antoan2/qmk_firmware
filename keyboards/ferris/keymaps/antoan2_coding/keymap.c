2dec51343e77c9f1550831eb0c276b0de4a30086
Antoine Biard <antoine.biard@padam.io>
2024-03-25 12:55:53 +0100

    move mouse to left hand and minor navigation updates

--- keyboards/ferris/keymaps/antoan2_coding/keymap.c
+++ keyboards/ferris/keymaps/antoan2_coding/keymap.c
@@ -29,15 +29,15 @@ enum layer_names {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BL] = LAYOUT(
-        KC_Q,         KC_W,         KC_E,           KC_R,         KC_T,          KC_Y, KC_U,         LT(4, KC_I),            KC_O,           KC_P,
+        KC_Q,         KC_W,         LT(4, KC_E),    KC_R,         KC_T,          KC_Y, KC_U,         KC_I,            KC_O,           KC_P,
         LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D),   LSFT_T(KC_F), KC_G,          KC_H, RSFT_T(KC_J), RCTL_T(KC_K),    LALT_T(KC_L),   RGUI_T(KC_SCLN),
         KC_Z,         KC_X,         KC_C,           KC_V,         KC_B,          KC_N, KC_M,         KC_COMM,         KC_DOT,         KC_SLSH,
                                     LT(3, KC_TAB), LT(1, KC_BACKSPACE),          LT(2, KC_SPACE), KC_ENTER
     ),
 	[_NAV] = LAYOUT(
-        KC_MEDIA_PREV_TRACK, KC_VOLU, KC_VOLD,             KC_MEDIA_NEXT_TRACK,   KC_TRNS,      KC_TRNS, KC_MS_WH_DOWN,  KC_MS_WH_UP, KC_TRNS,  KC_TRNS,
-        KC_LEFT_GUI,             KC_LEFT_ALT, KC_LEFT_CTRL,             KC_LEFT_SHIFT,               KC_TRNS,               KC_TRNS, KC_LEFT, KC_DOWN,      KC_UP,         KC_RIGHT,
-        KC_MEDIA_REWIND,     KC_MUTE, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_FAST_FORWARD, KC_TRNS,    KC_TRNS,           KC_HOME, KC_PAGE_DOWN, KC_PAGE_UP,    KC_END,
+        KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,  KC_VOLU,     KC_VOLD,                KC_TRNS,      KC_TRNS, KC_MS_WH_DOWN, KC_MS_WH_UP,  KC_TRNS,    KC_TRNS,
+        KC_LEFT_GUI,         KC_LEFT_ALT,       KC_LEFT_CTRL,        KC_LEFT_SHIFT,          KC_TRNS,      KC_TRNS, KC_DOWN,       KC_UP,      KC_LEFT,      KC_RIGHT,
+        KC_MEDIA_REWIND, KC_MEDIA_FAST_FORWARD,     KC_MUTE,     KC_MEDIA_PLAY_PAUSE,  KC_TRNS,      KC_TRNS, KC_HOME,       KC_PAGE_DOWN, KC_PAGE_UP, KC_END,
                                                            KC_TRNS,             KC_TRNS,               KC_TRNS, KC_TRNS
     ),
 	[_SYM] = LAYOUT(
@@ -48,15 +48,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NUM] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_0,    KC_1, KC_2, KC_3, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_0,    KC_4, KC_5, KC_6, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS,
                                                KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS
     ),
     [_MOUSE] = LAYOUT(
-        KC_TRNS,    KC_TRNS,  KC_MS_WH_UP,   KC_TRNS,     KC_TRNS,               KC_TRNS, KC_MS_BTN1, KC_TRNS, KC_MS_BTN2, KC_TRNS,
-        KC_MS_LEFT, KC_MS_UP, KC_MS_DOWN,    KC_MS_RIGHT, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,    KC_TRNS,  KC_MS_WH_DOWN, KC_TRNS,     KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                    KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS
+        KC_TRNS, KC_MS_BTN1, KC_TRNS, KC_MS_BTN2, KC_TRNS,             QK_DYNAMIC_TAPPING_TERM_UP,  KC_MS_WH_UP, KC_TRNS,       KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,             QK_DYNAMIC_TAPPING_TERM_DOWN,  KC_MS_UP,    KC_MS_DOWN,    KC_MS_LEFT,    KC_MS_RIGHT,
+        KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,             QK_DYNAMIC_TAPPING_TERM_PRINT,  KC_MS_WH_UP, KC_MS_WH_DOWN, KC_TRNS,     KC_TRNS,
+                                                  KC_TRNS,    KC_TRNS,             KC_TRNS,  KC_TRNS
     ),
 };
 
@@ -75,3 +75,18 @@ combo_t key_combos[] = {
     COMBO(lgui_combo, KC_LEFT_GUI), // Equivalent to nav modifier + LGUI
 };
 // clang-format on
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // If using the mouse, should be long...
+        case LT(4, KC_E):
+            return TAPPING_TERM + 500;
+        // LGUI and RGUI should be slower to activate for now
+        case LGUI_T(KC_A):
+            return TAPPING_TERM + 400;
+        case RGUI_T(KC_SCLN):
+            return TAPPING_TERM + 400;
+        default:
+            return TAPPING_TERM;
+    }
+}
--- keyboards/ferris/keymaps/antoan2_coding/rules.mk
+++ keyboards/ferris/keymaps/antoan2_coding/rules.mk
@@ -1,21 +1,4 @@
 COMBO_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 MOUSEKEY_ENABLE = yes
-
-QUICK_TAP_TERM = 0
-
-# Set the mouse settings to a comfortable speed/accuracy trade-off,
-# assuming a screen refresh rate of 60 Htz or higher
-# The default is 50. This makes the mouse ~3 times faster and more accurate
-MOUSEKEY_INTERVAL = 16
-# The default is 20. Since we made the mouse about 3 times faster with the previous setting,
-# give it more time to accelerate to max speed to retain precise control over short distances.
-MOUSEKEY_TIME_TO_MAX = 40
-# The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
-MOUSEKEY_DELAY = 100
-# It makes sense to use the same delay for the mouseweel
-MOUSEKEY_WHEEL_DELAY = 100
-# The default is 100
-MOUSEKEY_WHEEL_INTERVAL = 50
-# The default is 40
-MOUSEKEY_WHEEL_TIME_TO_MAX = 100
+DYNAMIC_TAPPING_TERM_ENABLE = yes
